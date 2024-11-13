6b8e8885244fb9e9de87baa579a5eeff072f4f17
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-13 13:55:06 -0600

    Add files via upload

---
+++ weatherService ts.ts
@@ -0,0 +1,46 @@
+
+
+interface WeatherResponse {
+    city: string;
+    list: Array<{
+        dt: number;
+        main: {
+            temp: number;
+            pressure: number;
+            humidity: number;
+        };
+        weather: Array<{
+            description: string;
+        }>;
+    }>;
+}
+
+
+class WeatherService {
+    private apiKey: string;
+    private baseUrl: string;
+
+    constructor(apiKey: string) {
+        this.apiKey = apiKey;
+        this.baseUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+    }
+}
+
+export default WeatherService;
+
+const weatherService = new WeatherService('YOUR_API_KEY');
+
+const getWeatherData = async () => {
+    
+
+}
+
+getWeatherData();
+
+const displayWeather = async (city: string) => {
+    try {
+      
+    } catch (error) {
+      console.error(error.message);
+    }
+}
